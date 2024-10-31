719b5e3cf00490a3932570ebd3fe84c5a7e941f5
Ajay Deekonda <133995637+Ajaydeekonda@users.noreply.github.com>
2024-10-31 13:03:25 +0530

    adjusted the UI

--- backend/database.js
+++ backend/database.js
@@ -1,11 +1,13 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
-// Create a pool of connections
+
+// Create a connection pool to the MySQL database
 const pool = mysql.createPool({
   host: 'localhost',
-  user: 'root',
-  password: '',
-  database: '',
+  user : 'root',
+  password: 'Dikonda9@',
+  database: 'users',
 });
 
 const getConnection = async () => {
